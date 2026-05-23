ebe52ecc258a14bf892acbea5f596dd4b7079d69
TUANNV <tuannv111004@gmail.com>
2026-05-23 10:50:06 +0700

    chore: configure supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TRACK_SP_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: ''
+  SUPABASE_URL: 'https://ibmwjtrmxohuryohyril.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_wCaK0aeJ4KEJCdAKuSMqlw_2TyuFt6F'
 };
